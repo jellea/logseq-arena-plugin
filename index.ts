ee30387bb58306fb3d60bf6664a454f934fc320c
Jelle Akkerman <jellea@users.noreply.github.com>
2021-12-25 14:40:04 +0100

    add auth modal

--- index.ts
+++ index.ts
@@ -5,126 +5,128 @@ import {registerSlashCommands} from './slash-commands';
 const Arena = require("are.na");
 
 async function main () {
-  // You might need to set the Arena personal token manually
-  // Generator Personal Access token here: https://dev.are.na/users/sign_in
-  // logseq.updateSettings({arenaToken:""})
-
   let apiToken = logseq.settings.arenaToken
   
-  logseq.provideStyle(styles)
+  if (!apiToken || apiToken.length != 64){
+    // Open Auth Modal
+    logseq.showMainUI()
+  }else{
+    logseq.provideStyle(styles)
 
-  const arena = new Arena({accessToken: apiToken});
+    let arena = new Arena({accessToken: apiToken});
 
-  registerSlashCommands()
+    registerSlashCommands()
 
-  // Needed to be able to open external links
-  // Use by adding the following html attributes data-url="https://are.na/block/${id}" data-on-click="openLink"
-  logseq.provideModel({
-    openLink(e) {
-      const { url } = e.dataset
-      logseq.App.openExternalLink(url)
-    }
-  })
-
-  logseq.provideModel({
-    addBlockReference(e) {
-      const { logseqBlockUuid, arenaBlockId } = e.dataset
-      logseq.Editor.insertBlock(logseqBlockUuid, `{{renderer :arena-block, https://www.are.na/block/${arenaBlockId}}}`, { before: false })
-    }
-  })
-  
-  function renderBlock(b, logseqUuid) {
-    let { id, image, title } = b
-    return image ? `
-          <div class="arena-block" data-url="https://are.na/block/${id}" data-on-click="openLink">
-            <div class="arena-block-content"><img src="${image.square.url}"></div>
-            <div class="arena-block-title">${title}</div>
-            ${ logseqUuid ? `<a data-arena-block-id="${id}" data-logseq-block-uuid="${logseqUuid}" data-on-click="addBlockReference" title="Add reference to block in Logseq"><i class="ti ti-layers-linked"></i></a>` : ''}
-          </div>
-        ` : `
-          <div class="arena-block" data-url="https://are.na/block/${id}" data-on-click="openLink">
-            <div class="arena-block-content">'${b.content}'</div>
-            <div class="arena-block-title">${title}</div>
-            ${ logseqUuid ? `<a data-arena-block-id="${id}" data-logseq-block-uuid="${logseqUuid}" data-on-click="addBlockReference" title="Add reference to block in Logseq"><i class="ti ti-layers-linked"></i></a>` : ''}
-          </div>
-        `
-  }
-
-  function renderErrorMessage(slot){
-    logseq.provideUI({
-      key: 'arena-error',
-      slot, template: `
-      <div class="arena-plugin-wrapper">
-        <h4>Error rendering are.na channel or block</h3>
-      </div>
-      `,
+    // Needed to be able to open external links
+    // Use by adding the following html attributes data-url="https://are.na/block/${id}" data-on-click="openLink"
+    logseq.provideModel({
+      openLink(e) {
+        const { url } = e.dataset
+        logseq.App.openExternalLink(url)
+      }
     })
-  }
 
-  function renderChannel(slot, payload) {
-    let [type, channelUrl] = payload.arguments
-
-    const regex = /https:\/\/w*\.?are\.na\/.+\/(.+)\/?/gm;
-    let slug = regex.exec(channelUrl.trim())[1]
+    logseq.provideModel({
+      addBlockReference(e) {
+        const { logseqBlockUuid, arenaBlockId } = e.dataset
+        logseq.Editor.insertBlock(logseqBlockUuid, `{{renderer :arena-block, https://www.are.na/block/${arenaBlockId}}}`, { before: false })
+      }
+    })
     
-    const renderBlockPartial = block => renderBlock(block, payload.uuid)
+    function renderBlock(b, logseqUuid) {
+      let { id, image, title } = b
+      return image ? `
+            <div class="arena-block" data-url="https://are.na/block/${id}" data-on-click="openLink">
+              <div class="arena-block-content"><img src="${image.square.url}"></div>
+              <div class="arena-block-title">${title}</div>
+              ${ logseqUuid ? `<a data-arena-block-id="${id}" data-logseq-block-uuid="${logseqUuid}" data-on-click="addBlockReference" title="Add reference to block in Logseq"><i class="ti ti-layers-linked"></i></a>` : ''}
+            </div>
+          ` : `
+            <div class="arena-block" data-url="https://are.na/block/${id}" data-on-click="openLink">
+              <div class="arena-block-content">'${b.content}'</div>
+              <div class="arena-block-title">${title}</div>
+              ${ logseqUuid ? `<a data-arena-block-id="${id}" data-logseq-block-uuid="${logseqUuid}" data-on-click="addBlockReference" title="Add reference to block in Logseq"><i class="ti ti-layers-linked"></i></a>` : ''}
+            </div>
+          `
+    }
 
-    arena.channel(slug).get().then(channel=>{
-      let blocks = channel.contents.map(renderBlockPartial).join("")
+    function renderErrorMessage(slot){
       logseq.provideUI({
-        key: `arena-channel`,
+        key: 'arena-error',
         slot, template: `
         <div class="arena-plugin-wrapper">
-          <h3 class="arena-chan-title ${channel.status}">Are.na channel: ${channel.title}</h3>
-          <p>${!channel.metadata ? '' : channel.metadata.description}</p>
-          <p>${channel.length} blocks - <a data-on-click="openLink" data-url="https://are.na/${channel.owner.slug}/${channel.slug}">open in are.na</a></p>
-          <p></p>
-          <div class="arena-block-grid">
-            ${blocks}
-          </div>
+          <h4>Error rendering are.na channel or block</h3>
         </div>
         `,
       })
-    }).catch((error) => {
-      renderErrorMessage(slot)
-    })
-  }
-
-  function renderSingleBlock(slot, payload){
-    let [type, blockUrl] = payload.arguments
-
-    const regex = /https:\/\/w*\.?are\.na\/block\/(.+)\/?/gm;
-    let slug = regex.exec(blockUrl.trim())[1]
-
-    arena.block(slug).get().then(block=>{
-      logseq.provideUI({
-        key: `arena-block`,
-        slot, template: `
-        <div class="arena-plugin-wrapper">
-          <h3 class="arena-chan-title">Are.na block: ${block.title}</h3>
-          <p><a data-on-click="openLink" data-url="${blockUrl}">open in are.na</a></p>
-          <div class="arena-block-grid">
-            ${renderBlock(block, undefined)}
-          </div>
-        </div>
-        `
-        ,
-      })
-    }).catch((error) => {
-      renderErrorMessage(slot)
-    })
-  }
-
-  logseq.App.onMacroRendererSlotted(async({slot, payload}) => {
-    let [type] = payload.arguments
-      
-    switch(type){
-      case ':arena-channel':
-        return renderChannel(slot, payload)
-      case ':arena-block':
-        return renderSingleBlock(slot, payload)
     }
-  })
+
+    function renderChannel(slot, payload) {
+      let [type, channelUrl] = payload.arguments
+
+      const regex = /https:\/\/w*\.?are\.na\/.+\/(.+)\/?/gm;
+      let slug = regex.exec(channelUrl.trim())[1]
+      
+      const renderBlockPartial = block => renderBlock(block, payload.uuid)
+
+      arena.channel(slug).get().then(channel=>{
+        let blocks = channel.contents.map(renderBlockPartial).join("")
+        logseq.provideUI({
+          key: `arena-channel`,
+          slot, template: `
+          <div class="arena-plugin-wrapper">
+            <h3 class="arena-chan-title ${channel.status}">Are.na channel: ${channel.title}</h3>
+            <p>${!channel.metadata ? '' : channel.metadata.description}</p>
+            <p>${channel.length} blocks - <a data-on-click="openLink" data-url="https://are.na/${channel.owner.slug}/${channel.slug}">open in are.na</a></p>
+            <p></p>
+            <div class="arena-block-grid">
+              ${blocks}
+            </div>
+          </div>
+          `,
+        })
+      }).catch((error) => {
+        renderErrorMessage(slot)
+      })
+    }
+
+    function renderSingleBlock(slot, payload){
+      let [type, blockUrl] = payload.arguments
+
+      const regex = /https:\/\/w*\.?are\.na\/block\/(.+)\/?/gm;
+      let slug = regex.exec(blockUrl.trim())[1]
+
+      arena.block(slug).get().then(block=>{
+        logseq.provideUI({
+          key: `arena-block`,
+          slot, template: `
+          <div class="arena-plugin-wrapper">
+            <h3 class="arena-chan-title">Are.na block: ${block.title}</h3>
+            <p><a data-on-click="openLink" data-url="${blockUrl}">open in are.na</a></p>
+            <div class="arena-block-grid">
+              ${renderBlock(block, undefined)}
+            </div>
+          </div>
+          `
+          ,
+        })
+      }).catch((error) => {
+        renderErrorMessage(slot)
+      })
+    }
+
+    logseq.App.onMacroRendererSlotted(async({slot, payload}) => {
+      let [type] = payload.arguments
+        
+      switch(type){
+        case ':arena-channel':
+          return renderChannel(slot, payload)
+        case ':arena-block':
+          return renderSingleBlock(slot, payload)
+      }
+    })
+  }
+  
 }
 
 logseq.ready(main).catch(console.error)
